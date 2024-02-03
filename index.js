476831fa9d22c949c6c912df5bf054f783fd36e1
David <davidrm901503@gmail.com>
2024-02-03 14:53:47 -0500

    drone yml2

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require('express');
 
 const app = express()
 
-app.get('/', (req, res) => res.send('y ahora qqsssss'))
+app.get('/', (req, res) => res.send('y ahorass qqsssss'))
 app.listen(3000, () => console.log('Server ready'))
