ef48652d3cfc0e0a7863d015a650c7643f4685cc
David <davidrm901503@gmail.com>
2024-02-03 18:11:55 -0500

    push 1

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require('express');
 
 const app = express()
 
-app.get('/', (req, res) => res.send('y ahorass qqsssssss'))
+app.get('/', (req, res) => res.send('cambio para q '))
 app.listen(3000, () => console.log('Server ready'))
